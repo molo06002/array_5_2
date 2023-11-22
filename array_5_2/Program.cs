7e867fa1eeaabd8199bd305ee08564eff585511c
molo06002 <molo06002@E-R90YE07N>
2023-11-22 12:44:36 +0100

    Add project files.

---
+++ array_5_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace array_5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett tal: ");
+            string tal1 = Console.ReadLine();
+            int tal1_1 = Convert.ToInt32(tal1);
+            int[] tal = new int[3];
+            tal[0] = 1;
+            tal[1] = 2;
+            tal[2] = tal1_1 ;
+            Console.WriteLine("Alla tal: " + tal[0] + " ," + tal[1] + " ," + tal[2]);
+        }
+    }
+}
